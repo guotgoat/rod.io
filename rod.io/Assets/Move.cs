c3b47cc1f1c56313b6247c5cb942372fd4ae54b7
guotgoat <guotgoat@github.com>
2018-11-03 19:13:08 -0400

    i got a virus in here :)
    basics gui

---
+++ rod.io/Assets/Move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour 
+{
+    public float Speed;
+    public int Points;
+	void Update () 
+    {
+        Vector3 Target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Target.z = transform.position.z;
+
+        transform.position = Vector3.MoveTowards(transform.position, Target, Speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(Vector3.back * 10*Speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(Vector3.forward * 10*Speed * Time.deltaTime);
+      
+    }
+
+    
+}
+
---
+++ rod.io/Assets/Skyline/RandomPlayers.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPlayers : MonoBehaviour 
+{
+    public float Speed;
+    public int Points;
+    void Update () 
+    {
+        /*Vector3 Target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Target.z = transform.position.z;
+
+        transform.position = Vector3.MoveTowards(transform.position, Target, Speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(Vector3.back * 10*Speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(Vector3.forward * 10*Speed * Time.deltaTime);
+            */
+      
+    }
+
+    void OnTriggerEnter()
+    {
+
+        gameObject.SetActive(false);
+    }
+}
+
